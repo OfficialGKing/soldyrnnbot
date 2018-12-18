e6fdb7653e3de6849046f3ebb4cfcfcc918e4a72
Gregory King <webddesign101@gmail.com>
2018-12-18 15:08:59 -0800

    Edit member = message.author

--- commands/consultations-on.js
+++ commands/consultations-on.js
@@ -1,3 +1,7 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const bot = require('./bot.json');
+
 if(cmd ===  `${prefix}consultation-on`) {
     let perms = message.member.permissions;
     let has_admin = perms.has("ADMINISTRATOR");
@@ -6,7 +10,7 @@ if(cmd ===  `${prefix}consultation-on`) {
     if(has_admin == true || has_designer == true){
         let role = message.guild.roles.find(r => r.name === "Open for Consultations");
         let servericon = message.guild.iconURL;
-        let member = message.guild.member;
+        let member = message.author;
         member.addRole(role).catch(console.error);
         let roleemebed = new Discord.RichEmbed()
         .setDescription("You've been added to the role Open for Consultations")
